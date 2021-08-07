fc1d7cff4b3cad4b2d54d2d78b14623dcc5e14c8
shangxuebo <shangxuebo@ybm100.com>
2021-08-07 20:11:35 +0800

    作业

---
+++ org.u.study/chapterone/homework_01.py
@@ -0,0 +1,28 @@
+
+# 1，选择题
+num = int(input('请输入一个浮点数'))
+print(type(num))
+#选择A 整形
+
+# 2，选择图
+# print(f'名字是：{'we'}',年龄是：{20}')
+# print(f'名字是：{"we"}',年龄是：{20}')
+print(f"名字是：{'we'},年龄是：{20}")
+# print(f"名字是：{"we"},年龄是：{20}")
+# 选择C，C不报错
+
+# 3，编程题
+s1 = 'I love python'
+s2 = 'python love you'
+s3 = '***************'
+print(f'{s1}\n{s2}\n{s3}\n{s1}\n{s2}\n{s3}')
+
+
+# 4, 用户输入一个变量name显示到控制台
+name = input('请输入一个name')
+print(name);
+
+# 5，填空题  是0
+a = 10
+print(a %2)
+
