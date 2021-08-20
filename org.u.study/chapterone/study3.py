1213dc1431506c0126afc1d48b29862e71500107
shangxuebo <shangxuebo@ybm100.com>
2021-08-20 08:55:43 +0800

    练习

--- org.u.study/chapterone/study3.py
+++ org.u.study/chapterone/study3.py
@@ -1,16 +1,35 @@
 
-list = []
+list1 = list()
 
-list.append(1)
-list.append(2)
+list1.append(1)
+list1.append(2)
 
-list2 = sorted(list)
+list2 = sorted(list1)
 
-print(len(list))
+print(len(list1))
+print(f'原list:{list2}; 新list：{list1}')
+
+list1.reverse()
 print(f'原list:{list2}; 新list：{list}')
 
-#list.reverse()
-#print(f'原list:{list2}; 新list：{list}')
+list1.sort()
+print(f'原list:{list2}; 新list：{list1}')
 
-list.sort()
-print(f'原list:{list2}; 新list：{list}')
+
+tup = tuple()
+print(len(tup))
+
+
+dict1 = dict()
+dict2 = {}
+
+dict1['name'] = '麦芽'
+dict1['age'] = '30'
+dict1['sex'] = '女'
+print(dict1)
+print(len(dict1))
+
+
+set1 = set()
+set1.add(123)
+print(set1)
